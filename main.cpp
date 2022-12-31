d32239a5cbc63cf40e9e5d0b08b5dcb7c86f55a6
noubuddy <anton.nich00@gmail.com>
2022-12-31 18:28:18 +0200

    edit main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,31 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "} ";
+
 int main()
 {
     const int width = 1000;
     const int height = 1000;
-    
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
     /*--------------------------------------------INITIALIZATION----------------------------------------------------*/
     // init glfw
     glfwInit();
@@ -15,7 +35,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     // tell glfw we are using the CORE profile so we only have the modern functions
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    
+
     /*---------------------------------------------CREATE WINDOW----------------------------------------------------*/
     // create window
     GLFWwindow* window = glfwCreateWindow(width, height, "Window", nullptr, nullptr);
@@ -26,7 +46,7 @@ int main()
         glfwTerminate();
         return -1;
     }
-    
+
     // introduce the window into the current context
     glfwMakeContextCurrent(window);
     // load GLAD so it configures OpenGL
@@ -34,6 +54,45 @@ int main()
     // specify the viewport of OpenGl in the window
     glViewport(0, 0, width, height);
 
+    // init vertex buffer object and bind it
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    std::cout << "Compiling shader..." << std::endl;
+    
+    // init vertex shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    // compile vertex shader
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // init fragment shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // compile fragment shader
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // init shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    // link shaders with shader program
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    std::cout << "Shader compilation finished!" << std::endl;
+
+    glUseProgram(shaderProgram);
+
+    
+    
     /*-----------------------------------------------MAIN LOOP-------------------------------------------------------*/
     // keep the window opened / main loop
     while (!glfwWindowShouldClose(window))
@@ -42,7 +101,7 @@ int main()
         glClearColor(0.29f, 0.66f, 0.87f, 1.0f);
         // clean the back buffer and assign the new color to it
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
+
         // swap the back buffer with the front buffer
         glfwSwapBuffers(window);
         // process all the pooled events / make the window responding / take care of all GLFW event
@@ -50,6 +109,10 @@ int main()
     }
 
     /*--------------------------------------------------TERMINATION--------------------------------------------------*/
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
     // destroy window before ending the program
     glfwDestroyWindow(window);
 
